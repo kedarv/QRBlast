b4b81c90ee18cf8b959009c7da8011921235587b
Kedar <kedar54@gmail.com>
2015-02-28 15:32:10 -0600

    asynctask

--- app/src/main/java/com/example/kedar/qrblast/QRGenerator.java
+++ app/src/main/java/com/example/kedar/qrblast/QRGenerator.java
@@ -3,6 +3,7 @@ package com.example.kedar.qrblast;
 import android.app.Activity;
 import android.graphics.Bitmap;
 import android.graphics.Color;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Base64;
 import android.util.Log;
@@ -33,34 +34,62 @@ public class QRGenerator extends Activity {
             splitted.add(text.substring(i, Math.min(i + 500,text.length())));
             i += 500;
         }
-        for (String str : splitted) {
-            byte[] data = null;
-            try {
-                data = str.getBytes("UTF-8");
-            } catch (UnsupportedEncodingException e1) {
-                e1.printStackTrace();
-            }
-            String base64 = Base64.encodeToString(data, Base64.DEFAULT);
-            Log.w("base64", base64);
-            QRCodeWriter writer = new QRCodeWriter();
-            try {
-                BitMatrix bitMatrix = writer.encode(base64, BarcodeFormat.QR_CODE, 512, 512);
-                int width = bitMatrix.getWidth();
-                int height = bitMatrix.getHeight();
-                Bitmap bmp = Bitmap.createBitmap(width, height, Bitmap.Config.RGB_565);
-                for (int x = 0; x < width; x++) {
-                    for (int y = 0; y < height; y++) {
-                        bmp.setPixel(x, y, bitMatrix.get(x, y) ? Color.BLACK : Color.WHITE);
-                    }
-                }
-                ((ImageView) findViewById(R.id.qr_result)).setImageBitmap(bmp);
-                Log.w("info", "switched");
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
+        new LongOperation().execute(splitted);
     }
 
+    private class LongOperation extends AsyncTask <ArrayList<String>, Void, String> {
+
+        @Override
+        protected String doInBackground(ArrayList<String>... params) {
+            ArrayList<String> splitted = params[0];
+            for (String str : splitted) {
+                byte[] data = null;
+                try {
+                    data = str.getBytes("UTF-8");
+                } catch (UnsupportedEncodingException e1) {
+                    e1.printStackTrace();
+                }
+                String base64 = Base64.encodeToString(data, Base64.DEFAULT);
+                Log.w("base64", base64);
+                QRCodeWriter writer = new QRCodeWriter();
+                try {
+                    BitMatrix bitMatrix = writer.encode(base64, BarcodeFormat.QR_CODE, 512, 512);
+                    int width = bitMatrix.getWidth();
+                    int height = bitMatrix.getHeight();
+                    final Bitmap bmp = Bitmap.createBitmap(width, height, Bitmap.Config.RGB_565);
+                    for (int x = 0; x < width; x++) {
+                        for (int y = 0; y < height; y++) {
+                            bmp.setPixel(x, y, bitMatrix.get(x, y) ? Color.BLACK : Color.WHITE);
+                        }
+                    }
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            ((ImageView) findViewById(R.id.qr_result)).setImageBitmap(bmp);
+                        }
+                    });
+
+                    Log.w("info", "switched");
+                    Thread.sleep(1000);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+            return "Executed";
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            // might want to change "executed" for the returned string passed
+            // into onPostExecute() but that is upto you
+        }
+
+        @Override
+        protected void onPreExecute() {}
+
+        @Override
+        protected void onProgressUpdate(Void... values) {}
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
