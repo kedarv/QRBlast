8b9364447465d967af8998cbde747a699a7b5b6b
Kedar <kedar54@gmail.com>
2015-02-28 13:24:09 -0600

    first

---
+++ app/src/main/java/com/example/kedar/qrblast/MainActivity.java
@@ -0,0 +1,67 @@
+package com.example.kedar.qrblast;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button bScan = (Button) findViewById(R.id.bScan);
+        final IntentIntegrator integrator = new IntentIntegrator(this);
+        bScan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                integrator.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE_TYPES);
+                integrator.setPrompt("Scan QR");
+                integrator.setResultDisplayDuration(0);
+                integrator.setScanningRectangle(500, 500);
+                integrator.setCameraId(0);  // Use a specific camera of the device
+                integrator.initiateScan();
+            }
+        });
+    }
+    public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        IntentResult scanResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, intent);
+        if (scanResult != null) {
+            String scanned = scanResult.getContents();
+            Log.w("scanned content:", scanned);
+            Log.w("info", "found code");
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
